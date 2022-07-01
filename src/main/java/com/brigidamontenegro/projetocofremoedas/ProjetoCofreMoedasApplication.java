a6dc7af82e2263403338630d015f1bee03a24248
brigidam <brigida.montenegrogualberto@gmail.com>
2022-07-01 20:44:36 -0300

    Criação do Projeto

---
+++ src/main/java/com/brigidamontenegro/projetocofremoedas/ProjetoCofreMoedasApplication.java
@@ -0,0 +1,13 @@
+package com.brigidamontenegro.projetocofremoedas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoCofreMoedasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoCofreMoedasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brigidamontenegro/projetocofremoedas/ProjetoCofreMoedasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brigidamontenegro.projetocofremoedas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoCofreMoedasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
