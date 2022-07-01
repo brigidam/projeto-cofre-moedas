00b7ff6749458c3a2029eed37b598eee31edadba
brigidam <brigida.montenegrogualberto@gmail.com>
2022-07-01 20:53:02 -0300

    Classe Moeda

---
+++ src/main/java/com/brigidamontenegro/projetocofremoedas/entities/Moeda.java
@@ -0,0 +1,21 @@
+package com.brigidamontenegro.projetocofremoedas.entities;
+
+public abstract class Moeda {
+	
+	private double valor;
+	
+	public Moeda(double valor) {
+		this.valor = valor;
+	}
+	
+	public double getValor( ) {
+		return valor;
+	}
+	
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	
+	public abstract double converter();
+
+}
