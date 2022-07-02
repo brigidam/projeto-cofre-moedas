5cc000f25869745908867ed204d6f5ff5792b7c7
brigidam <brigida.montenegrogualberto@gmail.com>
2022-07-01 21:47:00 -0300

    Método Remover

--- src/main/java/com/brigidamontenegro/projetocofremoedas/services/Cofrinho.java
+++ src/main/java/com/brigidamontenegro/projetocofremoedas/services/Cofrinho.java
@@ -1,16 +1,28 @@
 package com.brigidamontenegro.projetocofremoedas.services;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import com.brigidamontenegro.projetocofremoedas.entities.Moeda;
 
 public class Cofrinho {
-	
+
 	List<Moeda> listaMoedas = new ArrayList<>();
-	
 
 	public void adicionar(Moeda moeda) {
 		listaMoedas.add(moeda);
+	}
+
+	public void remover(Moeda moeda) {
+		Iterator<Moeda> it = listaMoedas.iterator();
+		while (it.hasNext()) {
+			Moeda m = it.next();
+			if (m.getClass() == moeda.getClass()) {
+				if (m.getValor() == moeda.getValor()) {
+					it.remove();
+				}
+			}
+		}
 	}
 }
