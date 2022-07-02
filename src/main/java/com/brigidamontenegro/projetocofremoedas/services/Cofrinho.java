2f1307e8be1081bcffa5c15098cd650a411b028b
brigidam <brigida.montenegrogualberto@gmail.com>
2022-07-01 22:17:37 -0300

    Método para converter o total do cofrinho em Real

--- src/main/java/com/brigidamontenegro/projetocofremoedas/services/Cofrinho.java
+++ src/main/java/com/brigidamontenegro/projetocofremoedas/services/Cofrinho.java
@@ -27,15 +27,15 @@ public class Cofrinho {
 			}
 		}
 	}
-	
+
 	public void listagemMoedas() {
 		int dolar = 0;
 		int euro = 0;
 		int real = 0;
-		for(Moeda moeda : listaMoedas) {
-			if(moeda instanceof Dolar) {
+		for (Moeda moeda : listaMoedas) {
+			if (moeda instanceof Dolar) {
 				dolar += moeda.getValor();
-			} else if(moeda instanceof Euro) {
+			} else if (moeda instanceof Euro) {
 				euro += moeda.getValor();
 			} else {
 				real += moeda.getValor();
@@ -43,5 +43,21 @@ public class Cofrinho {
 		}
 		System.out.println("Listando todas as Moedas dentro do Cofrinho");
 		System.out.print(dolar + " Dólares \n" + euro + " Euros \n" + real + " Reais \n");
+	}
+
+	public String totalConvertido() {
+		double sum = 0;
+		for (Moeda moeda : listaMoedas) {
+			if (moeda instanceof Dolar) {
+				double dolarParaReal = moeda.converter();
+				sum += dolarParaReal;
+			} else if (moeda instanceof Euro) {
+				double euroParaReal = moeda.converter();
+				sum += euroParaReal;
+			} else {
+				sum += moeda.converter();
+			}
+		}
+		return String.format("O valor total em Real é: R$ %.2f ", sum);
 	}
 }
