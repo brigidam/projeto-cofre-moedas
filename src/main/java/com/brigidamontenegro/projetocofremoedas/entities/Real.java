4ce4339e38fce08896e779516fd4252fc7254614
brigidam <brigida.montenegrogualberto@gmail.com>
2022-07-01 21:27:47 -0300

    Implementação da Classe Real

--- src/main/java/com/brigidamontenegro/projetocofremoedas/entities/Real.java
+++ src/main/java/com/brigidamontenegro/projetocofremoedas/entities/Real.java
@@ -1,5 +1,13 @@
 package com.brigidamontenegro.projetocofremoedas.entities;
 
-public class Real {
+public class Real extends Moeda {
 
+	public Real(double valor) {
+		super(valor);
+	}
+
+	@Override
+	public double converter() {
+		return getValor();
+	}
 }
